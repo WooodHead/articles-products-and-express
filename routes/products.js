a61ea910cc5208784341a77ab3b6736f9036fc4b
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-11 22:04:26 -1000

    created db files to store data, able to store posts and display products

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,26 @@
 //jshint esversion: 6
 const express = require('express');
 const router = express('router');
+const products = require('../db/products');
 
+let id = 0;
 
+router.post('/', (req, res) => {
+  console.log(req.body);
+  if(req.body.hasOwnProperty('name') && req.body.hasOwnProperty('price') && req.body.hasOwnProperty('inventory')){
+    let productObject = {
+      id: id,
+      name: req.body.name,
+      price: req.body.price,
+      inventory: req.body.inventory
+    };
+    products.productList.push(productObject);
+    res.json({"success": true});
+  } else {
+    res.json({"success": false});
+  }
+  id++;
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 //jshint esversion: 6
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const products = require('./routes/products');
+const displayProducts = require('./db/products');
 
+app.use(bodyParser.urlencoded());
 app.use('/products', products);
 
 app.get('/', (req, res) => {
-  res.send("home directory");
+  res.send(displayProducts.productList);
 });
 
 module.exports = app;
