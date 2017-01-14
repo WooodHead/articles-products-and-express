ccf796e1de0fbeabe9a072a590ba5a85b5dcfd9c
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-14 00:35:29 -1000

    working on put in articles route

--- routes/articles.js
+++ routes/articles.js
@@ -18,11 +18,48 @@ router.post('/', (req, res) => {
     };
     articleMap[newArticle.title] = savedArticle;
     res.send(articleMap);
+  } else {
+    res.send("error");
   }
+});
+
+router.put('/:title', (req, res) => {
+  let newArticle = req.body;
+  let articlePath = req.params.title;
+  console.log(articlePath);
+  console.log(newArticle.title);
+  let storedArticle = articleMap[req.body.title];
+
+  if(titleIsValid(newArticle, articlePath)){
+    switch(true){
+      case newArticle.hasOwnProperty('title'):
+        storedArticle.title = newArticle.title;
+        break;
+      case newArticle.hasOwnProperty('body'):
+        storedArticle.body = newArticle.body;
+        break;
+      case newArticle.hasOwnProperty('author'):
+        storedArticle.author = newArticle.author;
+        break;
+      default:
+        res.send(' switch error');
+    }
+  } else {
+    res.send(' validation error');
+  }
+  res.send(articleMap);
 });
 
 function bodyIsValid(article) {
   if(article.hasOwnProperty('title') && article.hasOwnProperty('body') && article.hasOwnProperty('author')){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function titleIsValid(article, address) {
+  if(article.hasOwnProperty('title') && article.title === address && articleMap.hasOwnProperty(article.title)){
     return true;
   } else {
     return false;
