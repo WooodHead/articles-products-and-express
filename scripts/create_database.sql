c1a60dd39fdae355c440d94cbbd4671d704a1405
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-26 07:56:24 -1000

    trying to re arrange dtabase and tables

---
+++ scripts/create_database.sql
@@ -0,0 +1,24 @@
+\c matthewtirrell
+
+DROP DATABASE IF EXISTS articles_products_db;
+CREATE DATABASE articles_products_db;
+
+\c articles_products_db
+
+DROP TABLE IF EXISTS articles;
+CREATE TABLE articles
+(
+id serial PRIMARY KEY NOT NULL,
+title text NOT NULL,
+body text NOT NULL,
+author text NOT NULL
+);
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products
+(
+id serial PRIMARY KEY NOT NULL,
+name text NOT NULL,
+price integer NOT NULL,
+inventory integer NOT NULL
+);
