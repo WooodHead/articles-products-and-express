96a2a0a470703da455de39d4f032f610cf49e463
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-25 23:57:12 -1000

    set up articles and products database..trying to get pgpromise to work

--- db/articles.js
+++ db/articles.js
@@ -1,5 +1,15 @@
 let articleList = {};
 
+const PG_PASS = process.env.PG_PASS;
+const pgp = require('pg-promise')();
+const articlesdb = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'articles_db',
+    user: 'matthewtirrell',
+    password: PG_PASS
+});
+
 function getArticles() {
   return articleList;
 }
